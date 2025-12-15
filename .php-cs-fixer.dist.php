76212f13ba78d66cd387a3b3d3ec37950eb3f1d3
Clément <33451311+clmntgbr@users.noreply.github.com>
2025-12-15 11:47:27 +0100

    Add PHP quality assurance tools: PHPStan, PHP CS Fixer, and Rector with configurations and CI integration

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+    ->exclude('vendor')
+    ->exclude('config')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'declare_strict_types' => true,
+        'no_unused_imports' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'phpdoc_align' => ['align' => 'left'],
+        'phpdoc_order' => true,
+        'phpdoc_separation' => false,
+        'single_line_throw' => false,
+        'yoda_style' => false,
+    ])
+    ->setFinder($finder)
+    ->setRiskyAllowed(true)
+;
---
+++ rector.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+    ])
+    ->withSkip([
+        __DIR__ . '/var',
+        __DIR__ . '/vendor',
+    ])
+    ->withPhpSets(php84: true)
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true
+    )
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+    ])
+    ->withImportNames();
+
